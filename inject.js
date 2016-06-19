bf6847f77cc77db1de1315fef652b2877883997c
Lachlan McCarty <lachlan@lachm.com>
2016-06-19 02:37:31 -0400

    ES6, style, squashed a bug

--- inject.js
+++ inject.js
@@ -1,51 +1,81 @@
-(function code(){
-    var categories = {};
-    var weights = {};
-    var weighted  = false;
-    var fracpercents = {};
+const weights = {};
+let weighted  = false;
+const termRegex = /Assignment Scores \(([A-Za-z0-9]+)\)/;
+const termName = termRegex.exec($('.box-round').eq(2).children('h2').text())[1];
+const percentId = 'percent-calc-chrome'
 
-    var termRegex = /Assignment Scores \(([A-Za-z0-9]+)\)/
-    var termName = termRegex.exec(jQuery(".box-round:eq(2) h2").text())[1];
-
-    jQuery(".box-round:eq(3) table tbody").each(function (index, el) {
-      if (jQuery(el).children("tr:eq(0)").children("th:eq(0)").text().split(" ")[1] === termName) {
-        jQuery(el).children("tr").each(function (index, el) {
-          if (jQuery(el).children("td:eq(0)").text() === "Category Based") {
-            weights[jQuery(el).children("td:eq(1)").text()] = Number(jQuery(el).children("td:eq(2)").text());
-            weighted = true;
-          }
-        });
+// get weights from "Special Weighting"
+$('.box-round').eq(3).find('table tbody').each((index, elem) => {
+  const tr = $(elem).children('tr');
+  if (tr.eq(0).children('th').eq(0).text().split(' ')[1] === termName) {
+    tr.slice(1).each((index, elem) => {
+      const td = $(elem).children('td');
+      if (td.eq(0).text() === 'Category Based') {
+        weights[td.eq(1).text()] = Number(td.eq(2).text());
+        weighted = true;
       }
     });
+  }
+});
 
-    jQuery(".box-round:eq(0) table:eq(0) tr td:eq(3)").html(jQuery(".box-round:eq(0) table:eq(0) tr td:eq(3)").html()+" (<span id=\"percent-calc-chrome\"></span>%)");
+// add span for percentage
+$('.box-round').eq(0).children('table').eq(0).find('tr td').eq(3).append(' (<span id="' + percentId + '"></span>%)');
 
-    function calculate_percent() {
-	var total = 0;
-	var totalpossible = 0;
-	var sum = 0;
-	if (weighted) {
-	    jQuery(".box-round:eq(1) table tbody tr").each(function(index, el) {
-		categories[jQuery(el).children("td:eq(0)").text().split(" (")[0]] = Number(jQuery(el).children("td:eq(3)").text()) / Number(jQuery(el).children("td:eq(2)").text())
-	    });
+function calculatePercent () {
+  let sum = 0;
 
-	    for (i in categories) {
-		total += weights[i]
-	    }
-	    for (i in categories) {
-		fracpercents[i] = categories[i]*weights[i]/total;
-		sum += fracpercents[i]
-	    }
-	} else {
-	    jQuery(".box-round:eq(1) table tbody tr").each(function(index, el) {
-		total += Number(jQuery(el).children("td:eq(3)").text());
-		totalpossible += Number(jQuery(el).children("td:eq(2)").text())
-	    });
-	    sum = total/totalpossible;
-	}
-	jQuery("#percent-calc-chrome").text((sum*100).toFixed(2));
+  if (weighted) {
+    let totalWeights = 0;
+    const categories = {};
+    const fracPercents = {};
+
+    $('.box-round').eq(1).find('table tbody tr').each((index, elem) => {
+      const td = $(elem).children('td');
+      categories[td.eq(0).text().split(' (')[0]] = Number(td.eq(3).text()) / Number(td.eq(2).text());
+    });
+
+    for (i in categories) {
+      if (i in weights) {
+        totalWeights += weights[i];
+      } else {
+        // on the off-chance a teacher creates a category but doesn't give it a weight
+        weights[i] = 0;
+      }
     }
 
-    jQuery(".box-round:eq(1) tbody tr td").attr("contenteditable",true).keyup(calculate_percent);
-    calculate_percent();
-})();
+    for (i in categories) {
+      fracPercents[i] = categories[i] * weights[i] / totalWeights;
+      sum += fracPercents[i];
+    }
+  } else {
+    let totalPts = 0;
+    let totalPossible = 0;
+
+    $('.box-round').eq(1).find('table tbody tr').each((index, elem) => {
+      const td = $(elem).children('td');
+      totalPts += Number(td.eq(3).text());
+      totalPossible += Number(td.eq(2).text());
+    });
+    sum = totalPts / totalPossible;
+  }
+
+  $('#percent-calc-chrome').text((sum * 100).toFixed(2));
+}
+
+// make points fields editable and add calculatePercent for keyup event
+const ptsIndexes = [];
+$('.box-round').eq(1).find('thead tr th').each((index, elem) => {
+  if (elem.innerHTML === 'Points Possible' || elem.innerHTML === 'Points Earned') {
+    ptsIndexes.push(index);
+  }
+});
+$('.box-round').eq(1).find('tbody tr').each((index, elem) => {
+  $(elem).children('td').each((index, elem) => {
+    if (ptsIndexes.includes(index)) {
+      $(elem).attr('contenteditable', true)
+             .keyup(calculatePercent);
+    }
+  });
+});
+
+calculatePercent();
